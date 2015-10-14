bb3dfd0709d35b64b90d244f09475da91ec15a8f
AdachiAkinori <adachi.c.akinori@gmail.com>
2015-10-15 00:53:48 +0900

    first commit

---
+++ src/mapBuilderApp/GameMap.go
@@ -0,0 +1,10 @@
+package main
+
+type gameMap struct {
+	var maxX int
+	var maxY int
+	var maxZ int
+	var jungleGym [][][]GameParts
+}
+
+var gameMap gameMap
---
+++ src/mapBuilderApp/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+
+	//マップパーツ情報
+	//pre
+	//- [] パーツ情報のロード
+
+	//loop
+	//- [] アルゴリズムで自動生成
+	//- [] png生成->アップロード
+	//- [] jsonの生成->アップロード
+
+	//- [] エディタでjsonロード
+
+
+}
